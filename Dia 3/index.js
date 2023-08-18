99fa08d06f98a6a6745878ec104a00f019df2e35
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-18 11:27:09 -0300

    Reajustando Repositorio dos 21 dias

---
+++ Dia 3/index.js
@@ -0,0 +1,18 @@
+
+let nome = "André"
+let idade = 18
+let altura = 1.80
+let peso = 72
+
+nome = prompt("Digite seu Nome: ")
+idade= parseFloat(prompt("DIgite sua Idade: "))
+altura= prompt("Digite sua Altura")
+peso= prompt("Digite seu Peso")
+
+let AnoQueNasci = 2005
+AnoNascido = 2023-idade
+
+let IMC = 0
+IMC= 72 / (1.8*1.8)
+
+console.log ("Olá" + nome + ", eu tenho" + idade + "anos, nasceu em" + AnoQueNasc +", tem" + altura + "de altura, pesa" + peso +"kg e seu IMC é" + IMC + "kg/m2")
