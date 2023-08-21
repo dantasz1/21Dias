1c7ee4c539bd98efa37b151c1abddc26da275dec
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-21 17:04:12 -0300

    Operadores & e ||

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,23 +1,12 @@
-let idade = Number(prompt("Insira sua Idade"))
-if( idade <=9){
-    console.log(idade)
-console.log("Vc tem MAIS que 9 anos")
-if(true){}
-var novinhodograu = "Andrezao"
-console.log(novinhodograu)}
-else{
-    
-console.log("vc tem menos q 9 anos")
-    }
-console.log( "fora do escopo do if/else")
-console.log()
-// ============================================
-// if = se | else= se nao
-// essas barras sao ( lembretes)
-// == igual | != diferente | !== (compara se é diferente o valor)
-// < menor que e > maior que 
-//| <= ou >= | === ex; 2==="2"
-//(nao quero comparar so o valo mas o tipo tambem)
-// essas barras sao ( lembretes)
-// == igual | != diferente | !== (compara se é diferente o valor) < menor que e > maior que 
-//| <= ou >= | === ex; 2==="2"(nao quero comparar so o valo mas o tipo tambem
+let idade = Number(prompt("Insira sua idade"))
+nome = prompt ("Insira seu Nome")
+if(idade === 18 && nome ==="André"){
+    console.log("Seu nome é André E voce tem 18 anos")
+}
+if(idade === 18 || nome=== "André"){
+    console.log( "Vc tem 18 anos OU seu nome é André")
+}else{
+console.log("Seu nome nao é Andre e vc nao tem 18 anos")
+
+}
+// (&&) - E |  (||)- OU | else if = (se nao se)
