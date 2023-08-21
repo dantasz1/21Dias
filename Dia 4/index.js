a9105bac75eb6a1bf1b31a562badab6329594719
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-21 16:19:54 -0300

    Dia-4, IF/ELSE;escopo

---
+++ Dia 4/index.js
@@ -0,0 +1,23 @@
+let idade = Number(prompt("Insira sua Idade"))
+if( idade <=9){
+    console.log(idade)
+console.log("Vc tem MAIS que 9 anos")
+if(true){}
+var novinhodograu = "Andrezao"
+console.log(novinhodograu)}
+else{
+    
+console.log("vc tem menos q 9 anos")
+    }
+console.log( "fora do escopo do if/else")
+console.log()
+// ============================================
+// if = se | else= se nao
+// essas barras sao ( lembretes)
+// == igual | != diferente | !== (compara se é diferente o valor)
+// < menor que e > maior que 
+//| <= ou >= | === ex; 2==="2"
+//(nao quero comparar so o valo mas o tipo tambem)
+// essas barras sao ( lembretes)
+// == igual | != diferente | !== (compara se é diferente o valor) < menor que e > maior que 
+//| <= ou >= | === ex; 2==="2"(nao quero comparar so o valo mas o tipo tambem
