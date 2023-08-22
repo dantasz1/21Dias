a76c75b98ddcd8cc9a43d9a360b6182db3fbf020
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-22 20:17:35 -0300

    Finalizando o exercicio do dia-4

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,10 +1,19 @@
-let  questionUsuarioFome =String(prompt ("Voce esta com Fome??"))
-let questionUsuarioMoney =String(prompt("Voce tem dinheiro??"))
-let local = String (prompt("O Restaurante esta aberto??"))
+let  questionUsuarioFome =prompt ("Voce esta com Fome?? Sim/Não")
+let questionUsuarioMoney =prompt("Voce tem dinheiro?? Sim/Não")
+let local =prompt("O Restaurante esta aberto?? Sim/Não")
 
-questionUsuarioFome = false
-questionUsuarioMoney = false
-local = false
+if(questionUsuarioFome =="Sim" && questionUsuarioMoney=="Não"){
+console.log("Hoje a janta sera em casa")
+}else if (questionUsuarioFome =="Sim" && questionUsuarioMoney == "Sim" && local =="Não") {
+console.log("Peça um delivery.")
+}else if(questionUsuarioFome =="Sim" && questionUsuarioMoney =="Sim" && local =="Sim"){
+    console.log("Hoje a janta sera no seu restaurante preferido")
+}
+
+    
+
+
+
 
 
 
