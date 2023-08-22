69026779362bfac06b6c2af0cf65d051d965deba
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-22 00:12:36 -0300

    fazendo o exercicio

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,12 +1,11 @@
-let idade = Number(prompt("Insira sua idade"))
-nome = prompt ("Insira seu Nome")
-if(idade === 18 && nome ==="André"){
-    console.log("Seu nome é André E voce tem 18 anos")
-}
-if(idade === 18 || nome=== "André"){
-    console.log( "Vc tem 18 anos OU seu nome é André")
-}else{
-console.log("Seu nome nao é Andre e vc nao tem 18 anos")
+let  questionUsuarioFome =String(prompt ("Voce esta com Fome??"))
+let questionUsuarioMoney =String(prompt("Voce tem dinheiro??"))
+let local = String (prompt("O Restaurante esta aberto??"))
 
-}
-// (&&) - E |  (||)- OU | else if = (se nao se)
+questionUsuarioFome = false
+questionUsuarioMoney = false
+local = false
+
+
+
+   
