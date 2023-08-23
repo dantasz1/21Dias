de9d60133ce1d2d0436e500283e3d04d0d51195c
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-23 16:52:51 -0300

    Finalizando o desafio #1 e #2

--- Dia 4/desafio1.js
+++ Dia 4/desafio1.js
@@ -1,21 +1,16 @@
 let  questionUsuarioFome =(prompt ("Voce esta com Fome?? Sim/Não"))
 let questionUsuarioMoney =(prompt("Voce tem dinheiro?? Sim/Não"))
-let local =(prompt("O Restaurante esta aberto?? Sim/Não"))
 
-if(questionUsuarioFome =="Sim" && questionUsuarioMoney=="Não"){
-console.log("Hoje a janta sera em casa")
-}else if (questionUsuarioFome =="Sim" && questionUsuarioMoney == "Sim" && local =="Não") {
-console.log("Peça um delivery.")
-}else if(questionUsuarioFome =="Sim" && questionUsuarioMoney =="Sim" && local =="Sim"){
-    console.log("Hoje a janta sera no seu restaurante preferido")
-}
 
-if (condition) {
-    
+if (questionUsuarioFome =="Não" || questionUsuarioMoney == "Não") {
+    console.log("hoje a janta sera em casa")
 }else{
-    if (condition) {
-        
-    }else if(Condicao){
-
+    let local =(prompt("O Restaurante esta aberto?? Sim/Não"))
+    
+    
+    if (questionUsuarioFome == "Sim" && questionUsuarioMoney == "Sim" && local == "Não") {
+        console.log("Peça um delivery")
+    }else if(questionUsuarioFome == "Sim" && questionUsuarioMoney == "Sim" && local == "Sim"){
+       console.log("Hoje a janta sera no seu restaurante preferido!")
     }
 }
--- Dia 4/desafio2.js
+++ Dia 4/desafio2.js
@@ -2,22 +2,24 @@ let InforNome =prompt("Insira seu Nome!")
 let InforIdade =prompt("Insira sua Idade!")
 let InforCart =prompt("Voce possui carteira de motorista? | Sim/Não")
 let InforCar = prompt("Voce tem algum carro? | Sim/Não")
+
 if(InforIdade == "Não" || InforCart == "Não"){
     console.log("André, voce não pode dirigir.")
-    console.log("Voce tem Menos ou tem 18 anos")
-    InforIdade <=18} || InforIdade=>=18
-else{
-    console.log("Voce nao tem carteira ou idade")
+}if(InforIdade == "Sim" && InforCart == "Sim" && InforCar == "Não"){
+console.log("Voce pode dirigir mas nao tem carro!")
+}if(InforIdade == "Sim" && InforCart == "Sim"&& InforCar =="Sim"){
+    console.log("André, voc sera o motorista!")
 }
+
+
     
+
+    // DESAFIO #2 | DIA 4!
     
 
 
-if(InforIdade == "Sim" && InforCart == "Sim" && InforCar == "Não"){
-console.log("André, voce pode dirigir mas não tem um carro.")
-}else if (InforIdade == "Sim" && InforCart == "Sim"&& InforCar == "Sim") {
-    console.log("André, voce sera o motorista!.")
-}
+
+
 
 
 
