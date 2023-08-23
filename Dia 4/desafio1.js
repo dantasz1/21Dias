a2dbe0e668e62b93a04152e15c951522ef1ca01f
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-23 12:18:11 -0300

    Desafio 1 e 2

---
+++ Dia 4/desafio1.js
@@ -0,0 +1,21 @@
+let  questionUsuarioFome =(prompt ("Voce esta com Fome?? Sim/Não"))
+let questionUsuarioMoney =(prompt("Voce tem dinheiro?? Sim/Não"))
+let local =(prompt("O Restaurante esta aberto?? Sim/Não"))
+
+if(questionUsuarioFome =="Sim" && questionUsuarioMoney=="Não"){
+console.log("Hoje a janta sera em casa")
+}else if (questionUsuarioFome =="Sim" && questionUsuarioMoney == "Sim" && local =="Não") {
+console.log("Peça um delivery.")
+}else if(questionUsuarioFome =="Sim" && questionUsuarioMoney =="Sim" && local =="Sim"){
+    console.log("Hoje a janta sera no seu restaurante preferido")
+}
+
+if (condition) {
+    
+}else{
+    if (condition) {
+        
+    }else if(Condicao){
+
+    }
+}
