8f8c3d7ce40f8a62e0fc41d244cc848358aff3b9
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-23 01:11:56 -0300

    fazendo o desafio #2, dia 4

--- Dia 4/index.js
+++ Dia 4/index.js
@@ -1,14 +1,25 @@
-let  questionUsuarioFome =prompt ("Voce esta com Fome?? Sim/Não")
-let questionUsuarioMoney =prompt("Voce tem dinheiro?? Sim/Não")
-let local =prompt("O Restaurante esta aberto?? Sim/Não")
-
-if(questionUsuarioFome =="Sim" && questionUsuarioMoney=="Não"){
-console.log("Hoje a janta sera em casa")
-}else if (questionUsuarioFome =="Sim" && questionUsuarioMoney == "Sim" && local =="Não") {
-console.log("Peça um delivery.")
-}else if(questionUsuarioFome =="Sim" && questionUsuarioMoney =="Sim" && local =="Sim"){
-    console.log("Hoje a janta sera no seu restaurante preferido")
+let InforNome =prompt("Insira seu Nome!") 
+let InforIdade =prompt("Insira sua Idade!")
+let InforCart =prompt("Voce possui carteira de motorista? | Sim/Não")
+let InforCar = prompt("Voce tem algum carro? | Sim/Não")
+if(InforIdade == "Não" || InforCart == "Não"){
+    console.log("André, voce não pode dirigir.")
+    console.log("Voce tem Menos ou tem 18 anos")
+    InforIdade <=18} || InforIdade=>=18
+else{
+    console.log("Voce nao tem carteira ou idade")
 }
+    
+    
+
+
+if(InforIdade == "Sim" && InforCart == "Sim" && InforCar == "Não"){
+console.log("André, voce pode dirigir mas não tem um carro.")
+}else if (InforIdade == "Sim" && InforCart == "Sim"&& InforCar == "Sim") {
+    console.log("André, voce sera o motorista!.")
+}
+
+
 
     
 
