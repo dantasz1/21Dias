756089ce35b50667e7652dd0fee11c2204fe36b4
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-23 20:20:42 -0300

    exercicio

---
+++ Dia 6/desafiio1.js
@@ -0,0 +1,10 @@
+let numero =parseInt(prompt("Escolha um numero inteiro e positivo"))
+
+
+
+for(let contador = 0; contador <=50; contador++){
+console.log(numero + contador*5)
+}
+
+
+//console.log(numero + "x"+ contador +"=" + numero*contador) 
