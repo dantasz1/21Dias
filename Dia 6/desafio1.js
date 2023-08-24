275ebb8fe53204f0f224411a1c9258a4693ed6c3
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-24 00:20:05 -0300

    Dia 6 #1 e #2

---
+++ Dia 6/desafio1.js
@@ -0,0 +1,8 @@
+let numero =parseInt(prompt("Escolha um numero inteiro e positivo"))
+
+
+
+for(let contador = 0; contador <= 50; contador +=5){
+        console.log(contador)
+        }
+ 
---
+++ Dia 6/desafio2.js
@@ -0,0 +1,10 @@
+let tabuada = Number(prompt("Me fale algum numero"))
+
+
+for(let Cod = 50; Cod >= 0; Cod  -=5){
+    console.log(Cod)
+
+}
+    
+
+    
