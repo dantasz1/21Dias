86aa9bce2536eb5a8db33393a6d454031865fecf
André Luiz de Almeida Dantas <dantasluizandre@gmail.com>
2023-08-24 18:58:41 -0300

    fazendo desafio 1

--- Dia 7/dia7.js
+++ Dia 7/dia7.js
@@ -1,12 +1,76 @@
-let MediaGeral=0
-let Homens = 0
-let MulhresAcima7=0
-let MaiorHomensNotas=0
-let index = 1
+let mulheresAcimaDe7 = 0
+let MaiorNotaHomem = 0
+let NotaHomem = 0
+let MediaGeraLAlunos = 0
+let contagemDosAlunos = 1
 
-while (index <=10) {
-    let nota =
-    let sexo =    
 
+
+while (contagemDosAlunos) {
+    nota =parseInt(prompt("Insira sua nota" + contagemDosAlunos + "aluno"))
+    sexo=prompt("Qual seu sexo? m/f")
+
+ if (sexo==="m") {
+   if (nota > MaiorNotaHomem){
+
+   }NotaHomem++
+ }
+
+        
+    
+//}if (nao= "n"){
+//   contagemDosAlunos = false
+//}if(sexo ==="f"){
+//nota > mulheresAcimaDe7 && nota >=7)
+//onsole.log(mulheresAcimaDe7 + nota >=7)
 
 }
+
+
+
+
+
+
+ //    let continuar = ""
+//    let teste = true
+//    while (teste) {
+//        continuar = prompt("voce quer continuar? s/n")
+//        console.log(teste)
+//        if (continuar == "n") {
+//            teste = false
+//        }
+//    }
+
+    
+
+
+
+
+
+//let NotaHomem= 0
+//let contador =  1
+//while ( contador) {
+//    nota = parseInt (prompt("Coloque a nota do "+ contador + " aluno "))
+//    sexo = prompt("Qual o seu sexo? m/f")
+//    console.log(nota & sexo)
+//if(sexo == "m"){
+//if(sexo > NotaHomem ){
+//  sexo=NotaHomem
+//}
+//}
+
+
+
+//}
+
+
+//    let continuar = ""
+//    let teste = true
+//    while (teste) {
+//        continuar = prompt("voce quer continuar? s/n")
+//        console.log(teste)
+//        if (continuar == "n") {
+//            teste = false
+//        }
+//    }
+
