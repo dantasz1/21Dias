c58f87351ff4b1dc352c4f5ebbd11749fbbf25f9
André Luiz <dantasluizandre@gmail.com>
2023-08-30 00:34:02 -0300

    Dia 9

---
+++ Dia 9/desafio 1-1.js
@@ -0,0 +1,26 @@
+nome = prompt("Insira seu nome!")
+idade = prompt ("Insira sua idade!")
+peso = prompt ("Qual seu peso?")
+altura = prompt("Qual sua altura?")
+profissao = prompt("Qual profissao voce quer seguir?")
+console.log(" Ola " + nome + " , voce tem : " + idade + " , pesa " + peso + " sua profissao é : " + profissao) 
+if(idade >= 18){
+    console.log("Esta liberado para tomar umas geladas")
+}
+else if (idade < 18){
+console.log("Sem gelada para voce")
+}
+mes = idade * 30
+semanas = idade*7
+dias = idade*365
+console.log(" Sua idade em meses é : " + mes)
+console.log(" Sua idade em semanas é : " + semanas)
+console.log("Sua idade em dias é : " + dias)
+
+    
+
+
+    
+
+
+
---
+++ Dia 9/desasfio1-2.js
@@ -0,0 +1,4 @@
+peso = prompt("Qual seu peso?")
+altura = prompt("Qual sua altura??")
+IMC = 0;
+IMC = peso / altura*altura
