9dab0ef099d4dd97ea476f0b3dd6525a21a32c47
André Luiz <dantasluizandre@gmail.com>
2023-09-06 18:03:49 -0300

    fazendo o dia 10

---
+++ Dia 10/desafio1-3.js
@@ -0,0 +1,9 @@
+array = []
+contador = 0;
+continuar = true
+while (continuar) {
+    numeros = prompt("Quantos numeros deseja inserir no array?")
+array [contador] = numeros
+console.log(array )
+
+}
