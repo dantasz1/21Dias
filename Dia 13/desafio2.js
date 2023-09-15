f148ffcfaabef5ac7169b91c915e4c9cd3e49aa0
André Luiz <dantasluizandre@gmail.com>
2023-09-14 21:16:32 -0300

    fazendo

---
+++ Dia 13/desafio2.js
@@ -0,0 +1,29 @@
+class carro{
+Nome
+Potencia
+VelocidadeMaxima
+Aceleracao
+
+constructor(nome,potencia,velocidadeMaxima,aceleracao){
+this.Nome = nome
+this.Potencia = potencia
+this.VelocidadeMaxima =velocidadeMaxima
+this.Aceleracao = aceleracao
+}
+
+ApresentarInformacoes(){
+    console.log("Informaçoes do carro: ")
+console.log(`Nome: ${this.Nome}`)
+console.log(`Potencia : ${this.Potencia} cavalos `)
+console.log(`Velocidade maxima de : ${this.VelocidadeMaxima} KM/h`)
+console.log(`Aceleração de ate : ${this.Aceleracao} ate 100 KM/h`)
+}
+CalcularTempoMedio(distancia){
+    let resultado = (distancia) / this.VelocidadeMaxima /(this.Aceleracao)
+    return resultado
+    }
+}
+let classeCarros =new carro("Ferrari" , 800 , 320  , 2.5  )
+classeCarros.ApresentarInformacoes()
+let resultado = classeCarros.CalcularTempoMedio(1000)
+console.log(resultado);
---
+++ Dia 13/desafio3.js
@@ -0,0 +1,20 @@
+class Corrida{
+Nome
+Tipo
+Distancia
+Participantes
+Vencedor
+constructor(nome,tipo,distancia,participantes,vencedor){
+this.Nome= nome
+this.Tipo = tipo
+this.Distancia = distancia
+this.Participantes = participantes
+this.Vencedor = vencedor
+
+
+
+
+}
+
+
+}
