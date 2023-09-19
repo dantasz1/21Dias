a040f8a6de843b0fecdcacca109c1fc0860ba1e2
André Luiz <dantasluizandre@gmail.com>
2023-09-19 11:58:59 -0300

    gg

--- Dia 14/script.js
+++ Dia 14/script.js
@@ -1,8 +1,8 @@
-var nome
-var salario
+perguntas()
 function perguntas(){
     nome = prompt("Insira seu nome")
     salario = parseFloat(prompt("Qual seu salario?"))
+
     CalcularSalario(nome, salario)
 }
 function CalcularSalario(nome, salario) {
@@ -17,11 +17,21 @@ if (salario < 1500) {
 }else{
     aumento = 0.05
 }
+novosalario = salario + (salario*aumento)
 
+console.log("O nome dele é : " + nome)
+console.log(" O salario é : " + salario)
+console.log("O aumento de " + (aumento*100) + " % " )
+console.log("Salario reajustado: R$ " + novosalario)
 
+}
+ReajusteSalarial()
+function ReajusteSalarial () {
+    resposta = prompt("Vc deseja calcular novamente??")
+    if(resposta = "Sim"){
 
-
-
-
+    }else{
+        console.log("Programa acabou!")
+    }
 }
  
