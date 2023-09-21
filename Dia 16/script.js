f148735985b29dd221c19670a5eb6c957b8d2ff7
André Luiz <dantasluizandre@gmail.com>
2023-09-21 11:04:00 -0300

    ggs

--- Dia 16/script.js
+++ Dia 16/script.js
@@ -4,10 +4,10 @@ let senhas = []
 
 
 function Perguntas () {
-    let cadastros = prompt("| 1- Se vc deseja cadastrar | 2- Para fazer LOGIN | 3- Excluir um cadastro | Encerrar o cadastro")
+    let cadastros = prompt("| 1- Se vc deseja cadastrar | 2- Para fazer LOGIN | 3- Excluir um cadastro |4- Encerrar o cadastro")
    return cadastros
 }
-cadastroUsuario()
+
 function cadastroUsuario () {
     nomes.push(prompt("digite seu nome para o cadastro"))
 senhas.push(prompt("digite a senha que voce queria cadastrar"))
@@ -20,4 +20,42 @@ function FazerLogin(nome,senha) {
     } else {
         return false
     }
+}
+function ExclusaoCadastro(nome) {
+    let indice = nomes.indexOf(nome)
+if (indice !== -1) {
+    nomes.splice(indice , 1)
+    senhas.splice(indice , 1)
+console.log("cadastro excluido")
+}else{
+    console.log("usuario nao encontrado")
+}
+}
+let continuar = true
+while (continuar) {
+let opcao = Perguntas()
+switch (opcao) {
+    case "1":
+        cadastroUsuario()
+        break;
+case "2" : 
+let loginNome = prompt("digite seu nome")
+let loginSenha = prompt("digite sua senha")
+if (FazerLogin(loginNome, loginSenha)) {
+    console.log("login feito com sucesso")
+}else{
+    console.log("nome ou senha incorretos")
+}
+break;
+case "3" : 
+let nome = prompt("insira seu nome")
+ExclusaoCadastro(nome)
+break;
+case "4" : 
+continuar = false
+
+console.log("Encerrado com sucesso!")
+    default:
+        break;
+} 
 }
