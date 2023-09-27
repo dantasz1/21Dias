01e334469885a6a4d3314b2531ff4ce460893f54
André Luiz <dantasluizandre@gmail.com>
2023-09-27 16:21:22 -0300

    gg dia 17, chatinho

--- Dia 17/script.js
+++ Dia 17/script.js
@@ -35,6 +35,10 @@ function CadastroHotel() {
     categoria = prompt("Digite a categoria do hotel")
     endereco = prompt("Digite o endereço do hotel")
     telefone = prompt("Digite o telefone do hotel")
+    if (hoteis.some(hotel => hotel.Id === idHotel)) {
+        console.log("ID ja existe.Escolha outro ID")
+        return
+    }
     let objetoHotel = new Hotel(idHotel, nome, categoria, endereco, telefone)
     idHotel++
     hoteis.push(objetoHotel)
@@ -42,18 +46,30 @@ function CadastroHotel() {
 function CadastroReserva() {
 
     idHotel = prompt("digite o id do hotel")
+    if (!hoteis.some(hotel => hotel.Id === idHotel)) {
+        console.log("Id invalido. Por favor insira um Id valido")
+        return;
+    }
     nomeResponsavel = prompt("digite o nome do responsavel")
     DiaEntrada = prompt("Digite o dia da sua entrada em nosso hotel")
     DiaSaida = prompt("Digite o dia da sua saida em nosso hotel")
+    if (DiaSaida < DiaEntrada) {
+        console.log("O dia de entrada nao pode ser maior que o dia de saida!")
+    }
+    if (reservas.some(reserva => reserva.Id === idReserva)) {
+        console.log("Esse id da reserva ja existe. Escolha outro Id")
+        return ;
+    }
     let objetoReserva = new Reserva(idReserva, idHotel, nomeResponsavel, DiaEntrada, DiaSaida)
     idReserva++
     reservas.push(objetoReserva)
+
 }
 
 function IdDoHotel(idHotel) {
     reservas.forEach(reserva => {
         if (idHotel == reserva.IdDoHotel) {
-  //const filtroNomeHotel = hoteis.find((reserva) => reserva.Id == idHotel)
+                                                                    //const filtroNomeHotel = hoteis.find((reserva) => reserva.Id == idHotel)
 let i = idHotel - 1
             console.log("nome do hotel : "   + hoteis[i].Nome)
             console.log(`nome do responsavel é : ${reserva.NomeDoResponsavel}`)
@@ -90,12 +106,54 @@ function Categorias(categoria) {
     }
 }
 function NumeroAndID(idHotel, telefone){
-    
+    for (let i = 0; i < hoteis.length; i++) {
+       if (idHotel == hoteis[i].Id) {
+        hoteis[i].Telefone = telefone
+       }
+        
+    }
 }
+let continuar = true
+while (continuar) {
+    
 
+let opcao = prompt(" 1 - Para cadastrar | 2 - Para agendar sua reserva | 3 - Procurar reserva pelo ID | 4 - Procurar o hotel pela reserva" +
+ "5- Procurar pelo nome | 6 - Procurar pela categoria | 7 - Procurar pelo telefone | 8 - Encerrar programa ")
+switch (opcao) {
+    case '1': 
+        CadastroHotel()
+        break;
+case '2' : CadastroReserva()
+        break;
+case '3' : IdDoHotel(prompt("Digite o Id do hotel"))
+        break;
+case '4' : ProcurarHotelPelaReserva(prompt("Digite o Id da reserva"))
+        break;
+case '5' : Name(("Digite o nome do responsavel ")) 
+        break;
+case '6' : let AcharCategoriaHotel = Categorias(parseInt("Digite a categoria que deseja achar"))
+console.log(AcharCategoriaHotel)
+        break;  
+case '7' : let idDoHotel = NumeroAndID("Digite o numero do id q deseja atualizar")
+            let numero = prompt("digite seu novo telefone")
+NumeroAndID(idDoHotel,numero)
+        break;
+case '8' : 
+console.log("Programa encerrado")
+        continuar = false
+                break;
+    default:
+
+console.log("opçao invalida")
+        break;
+}
+}
 CadastroHotel() 
 CadastroReserva()
 IdDoHotel(1)
 ProcurarHotelPelaReserva(1)
 Name('andre')
-Categorias()
+Categorias(5)
+NumeroAndID(1 , 12345)
+
+
