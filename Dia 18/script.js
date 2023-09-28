466e87ccd50c8e4347c97a1fcc68acf3b5863aca
André Luiz <dantasluizandre@gmail.com>
2023-09-28 00:28:45 -0300

    começando

--- Dia 18/script.js
+++ Dia 18/script.js
@@ -4,28 +4,42 @@ class Livro {
     Editora
     AnoDePublicação
     Disponibilidade 
-    constructor(titulo, autor , editora, anoDePublicação,){
+    constructor(titulo, autor , editora, anoDePublicação,disponibilidade = true){
 this.Titulo = titulo
 this.Autor = autor 
 this.Editora = editora
 this.AnoDePublicação = anoDePublicação
-this.Disponibilidade = true 
+this.Disponibilidade = disponibilidade
     }
 }
 class Biblioteca{
     Nome
     Endereço
     Telefone
-    AcervoDeLivros
-    constructor(nome,endereco,telefone,acervoDeLivros){
+    AcervoDeLivros = []
+    constructor(nome,endereco,telefone,acervo){
         this.Nome = nome
         this.Endereço = endereco
         this.Telefone = telefone
-        this.AcervoDeLivros = acervoDeLivros
+        this.AcervoDeLivros = acervo
     }
-}
+          BuscarLivro(titulo) {
+            this.AcervoDeLivros.find(livro => { livro.Titulo == titulo
+                if (livro.Titulo == titulo) {
+                    console.log(livro)
+                }
+            });
+        }
+  
+  }
+
+        
+    
+
 arrayObjetosClassLivro = []
 arrayObjetosClassLivro.push(new Livro("Livro1" , "autor1" , "editora1" , 1920))
 arrayObjetosClassLivro.push(new Livro("Livro2" , "autor2" , "editora2" , 1930))
 arrayObjetosClassLivro.push(new Livro("Livro3" , "autor3" , "editora3" , 1940))
 arrayObjetosClassLivro.push(new Livro("Livro4" , "autor4" , "editora4" , 1950))
+MinhaBiblioteca = new Biblioteca("Nome1" , "Endereço1", "Telefone1" ,livro[0])
+BuscarLivro(this.AcervoDeLivros)
