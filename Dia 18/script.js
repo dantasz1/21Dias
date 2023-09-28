06994aa4c64e1a0ef3ec8d3be2e25c70cf099f02
André Luiz <dantasluizandre@gmail.com>
2023-09-28 19:39:35 -0300

    em progresso

--- Dia 18/script.js
+++ Dia 18/script.js
@@ -29,17 +29,44 @@ class Biblioteca{
                     console.log(livro)
                 }
             });
+        
+  function EmprestarLivro(titulo) {
+    let emprestar = false
+    this.AcervoDeLivros.forEach(livro => {
+    if (livro.Titulo == titulo) {
+        if (livro.Disponibilidade === true) {
+            livro.Disponibilidade = false
+            emprestar = true
         }
-  
+    }
+});
+if (emprestar ) {
+    return true
+}else{
+    return false
+}
+}
+  function DevolverLivro(titulo){
+this.AcervoDeLivros.forEach(livro => { livro.Titulo == titulo
+if (livro.Titulo == titulo) {
+    livro.Disponibilidade = true
+    console.log("Livro devolvido")
+}
+})
+  }
+  }
   }
 
-        
-    
 
 arrayObjetosClassLivro = []
 arrayObjetosClassLivro.push(new Livro("Livro1" , "autor1" , "editora1" , 1920))
 arrayObjetosClassLivro.push(new Livro("Livro2" , "autor2" , "editora2" , 1930))
 arrayObjetosClassLivro.push(new Livro("Livro3" , "autor3" , "editora3" , 1940))
 arrayObjetosClassLivro.push(new Livro("Livro4" , "autor4" , "editora4" , 1950))
-MinhaBiblioteca = new Biblioteca("Nome1" , "Endereço1", "Telefone1" ,livro[0])
-BuscarLivro(this.AcervoDeLivros)
+
+MinhaBiblioteca = new Biblioteca("Nome1" , "Endereço1", "Telefone1" )
+MinhaBiblioteca.BuscarLivro("Livro1")
+MinhaBiblioteca.EmprestarLivro("Livro2")
+MinhaBiblioteca.DevolverLivro("Livro3")
+
+
